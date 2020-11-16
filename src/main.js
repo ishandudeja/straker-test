ca6cb46d95812ed9258c91284442fdfe16b146cc
ishan dudeja <ishandudeja@gmail.com>
2020-11-17 08:38:30 +1300

    init coreui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import CoreuiVue from '@coreui/vue';
+import router from './router'
 
+Vue.use(CoreuiVue);
 Vue.config.productionTip = false
 
 new Vue({
+ 
+  router:router,
+  // store,
+  // icons,
   render: h => h(App),
 }).$mount('#app')
