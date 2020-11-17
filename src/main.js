dabdc6edfc10c08cc48bdcb44b8e4af6bed3d8a7
ishan dudeja <ishandudeja@gmail.com>
2020-11-17 22:07:34 +1300

    provide store access to components

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import CoreuiVue from '@coreui/vue';
 import router from './router'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 Vue.use(CoreuiVue);
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
-
+import store from './store'
 new Vue({
  
   router:router,
-  // store,
-  // icons,
-  render: h => h(App),
+   store,
+   render: h => h(App),
 }).$mount('#app')
